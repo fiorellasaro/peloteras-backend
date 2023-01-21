ca5db3fd99e5fb48ffbe3718f77575b224772c0f
fiorellasaro <fiorellasaro27@gmail.com>
2023-01-20 21:42:29 -0500

    add

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -15,7 +15,7 @@ import { CreateEventSchema } from "../schema/event.schema";
 router.get("/events", getEvents);
 
 router.get("/events/:id", getEvent);
- 
+
 router.post("/events", createEvent);
 
 router.delete("/events/:id", deleteEvent);
